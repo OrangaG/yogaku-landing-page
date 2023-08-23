4b9307c34296e85018b5832264291cbc73ed03d0
George Oranga <goranga46@gmail.com>
2023-08-23 21:13:15 +0300

    initial page setup done

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      "green-dark": "#9fd1c0",
+      green: "#b3f0da",
+      "gray-dark": "#463f58",
+      "gray-light": "#4d4f63",
+      white: "#f6f4f0",
+      white2: "#cbc9d1",
+    },
+    fontFamily: {
+      montserrat: ["Montserrat", "sans-serif"],
+      roboto: ["Roboto", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
